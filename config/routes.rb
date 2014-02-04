7d28be7711e0ded7fd45df84bd745c796fc453b8
Thuong Ho <thuong.t.ho@gmail.com>
2014-02-03 21:49:49 -0800

    change routes to #

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 FirstApp::Application.routes.draw do
+  
+  root 'welcome#index'
 
-  get "welcome/index"
-  root 'welcome/index'
+  get "welcomeindex"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
